8ff71561b9251392ee5a3b2e05aa4a7e1110c9ef
xa6u <skarabuta@curacubby.com>
2023-09-21 07:18:16 +0300

    Add new files: variables.js with homework2, gitignore and package.json

---
+++ js-variables/variables.js
@@ -0,0 +1,16 @@
+let adminName = 'Serhii';
+let userName = 1.0;
+
+console.log(typeof adminName);
+console.log(typeof userName);
+
+let awsome = 'awsome';
+
+let randomBoolVar = true;
+let randomBigInt = 700000n;
+
+console.log(`Javascript is: ${randomBoolVar},${randomBigInt}`);
+
+let changedVar = 'new';
+changedVar = 123;
+console.log(`New value of changedVar variable is: ${changedVar}`) 
